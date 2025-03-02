e88fc6edeec3c067389335497aa0bb71c1fa1f3d
nipaayasha05 <nipaayasha05@gmail.com>
2025-03-02 10:37:39 +0600

    adding bacground

--- script/change.js
+++ script/change.js
@@ -3,10 +3,13 @@ document.getElementById('change-btn').addEventListener('click', function(){
 
     const changeBtn = document.getElementById('change-btn')
     const bodyBg = document.getElementById('body-bg')
-   bodyBg.style.backgroundColor =  'rgb(204, 204, 255';
-
-  
+  //  bodyBg.style.backgroundColor =
+  bodyBg.style.backgroundColor = Math.floor(Math.random() * 256);;
+     console.log( changeBtn )
+    //  return 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')';
+  //  console.log(Math.random('rgb(r(204, 204, 255)+r(153, 153, 255))'));
+   bodyBg.style.backgroundColor = (Math.random()*255)
     console.log( changeBtn )
 });
-
+console.log( changeBtn )
  
