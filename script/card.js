f115490e4283d065e397cbedaa04732a0364d632
nipaayasha05 <nipaayasha05@gmail.com>
2025-03-02 20:59:43 +0600

    comment console.log

--- script/card.js
+++ script/card.js
@@ -2,7 +2,7 @@
     
     const cardBtn = document.querySelectorAll('.card-btn');
     let count=0;
-    
+     
 for(const btn of cardBtn){
     // console.log(btn)
     
@@ -25,14 +25,14 @@ for(const btn of cardBtn){
     let sum = 1;
     const taskNumber = parseInt(document.getElementById('task-number').innerText)
     task = taskNumber-sum;
-    console.log(task);
+    // console.log(task);
     document.getElementById('task-number').innerText = task;
 
-
+   
     
     const checkout = parseInt(document.getElementById('checkout-number').innerText)
     sum = checkout + sum;
-    console.log(sum);
+    // console.log(sum);
     document.getElementById('checkout-number').innerText = sum;
 
       
