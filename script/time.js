73f645a23c38e51e9541eaa8c233fed8238ecde2
nipaayasha05 <nipaayasha05@gmail.com>
2025-03-05 21:32:58 +0600

    edit date

--- script/time.js
+++ script/time.js
@@ -1,7 +1,11 @@
 const date = new Date();
 const currentDate = date.toDateString();
 const currentTime = date.toLocaleTimeString();
-//  console.log(currentDate);
+ 
+ const time =  currentDate.split(' ');
+ const day = time[0];
+ const time2 = `${time[1]} ${time[2]} ${time[3]}`
+//  console.log(time2)
 //  console.log(currentTime);
   
 
@@ -9,7 +13,8 @@ const currentTime = date.toLocaleTimeString();
  
  const p = document.createElement('p');
  p.innerHTML = `
- <h1 class="text-2xl text-black">${currentDate}</h1>
+ <h1 class="text-2xl text-black">${day} , <br>  <span class = "font-bold">${time2}</span>
+ </h1>
  `
 calender.appendChild(p)
 
